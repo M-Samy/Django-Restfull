a08e8471a3f471a29d47169c7e0786f3ed77a14f
n3rd <n3rd@n3rd>
2019-09-21 13:48:39 +0200

    Add date_range filter functionality

--- datasetapi/models.py
+++ datasetapi/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
+import datetime
 # Create your models here.
 
 
 class Dataset(models.Model):
     id = models.AutoField(primary_key=True)
-    date = models.CharField(max_length=10)
+    date = models.DateField(default=datetime.date.today)
     channel = models.CharField(max_length=16)
     country = models.CharField(max_length=10)
     os = models.CharField(max_length=10)
--- datasetapi/views.py
+++ datasetapi/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import Http404
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from . models import Dataset as DatasetModel
 from .serializers import DataSerializer
 from rest_framework.pagination import PageNumberPagination
-from rest_framework import filters
 from django.db.models import Q
+from datetime import datetime
 
 
 class Dataset(APIView):
@@ -21,8 +20,26 @@ class Dataset(APIView):
                     Q(country__icontains = query_param) |
                     Q(os__icontains = query_param)
                     )
+
+            from_date = self.request.query_params.get('from_date', None)
+            to_date = self.request.query_params.get('to_date', None)
+
+            dataset = self.get_data_within_a_daterange(from_date, to_date, dataset)
+
             result_page = paginator.paginate_queryset(dataset, request)
             serializer = DataSerializer(result_page, many=True)
             return paginator.get_paginated_response(serializer.data)
         except Exception as e:
+            print(e)
             raise Http404('Requested is failed with error {}'.format(e.__repr__))
+
+
+    def get_data_within_a_daterange(self, from_date, to_date, data):
+        try:
+            if from_date and to_date:
+                from_date = datetime.strptime(from_date,"%Y-%m-%d")
+                to_date = datetime.strptime(to_date,"%Y-%m-%d")
+                data = data.filter(date__range=[from_date, to_date])
+            return data
+        except Exception as e:
+            raise Http404('Failed filter data within a range of dates {}'.format(e.__repr__))
